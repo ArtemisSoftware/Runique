441b495dd88f06938f31c3ad9942bf72ff46f427
gustavo <gustavo.isaac@gmail.com>
2024-07-08 14:56:27 +0100

    String resource management

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -8,13 +8,8 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.ui)
+
     implementation(projects.core.domain)
     implementation(projects.core.presentation.designsystem)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
