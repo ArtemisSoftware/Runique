912b42a6a3fa9c14f208fb2003f41e1a0382d16d
gustavo <gustavo.isaac@gmail.com>
2024-07-09 17:20:48 +0100

    Navigation graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,19 +38,29 @@ dependencies {
     implementation(projects.run.location)
     implementation(projects.run.network)
 
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Compose
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    //implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+    //
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ app/src/main/java/com/artemissoftware/runique/RootNavGraph.kt
@@ -0,0 +1,56 @@
+package com.artemissoftware.runique
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.artemissoftware.auth.presentation.intro.IntroScreen
+import com.artemissoftware.auth.presentation.register.RegisterScreen
+
+@Composable
+fun RootNavGraph(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "auth"
+    ) {
+        authGraph(navController)
+    }
+}
+
+private fun NavGraphBuilder.authGraph(navController: NavHostController) {
+    navigation(
+        startDestination = "intro",
+        route = "auth"
+    ) {
+        composable(route = "intro") {
+            IntroScreen(
+                onSignUpClick = {
+                    navController.navigate("register")
+                },
+                onSignInClick = {
+                    navController.navigate("login")
+                }
+            )
+        }
+        composable(route = "register") {
+            RegisterScreen(
+                onSignInClick = {
+                    navController.navigate("login") {
+                        popUpTo("register") {
+                            inclusive = true
+                            saveState = true
+                        }
+                        restoreState = true
+                    }
+                },
+                onSuccessfulRegistration = {
+                    navController.navigate("login")
+                }
+            )
+        }
+    }
+}
